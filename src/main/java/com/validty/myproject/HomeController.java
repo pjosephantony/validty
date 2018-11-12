aff387cb1cf95b4988bbeb37e833816efd8cc665
joseph antony <antony.j@husky.neu.edu>
2018-11-12 13:14:39 -0500

    distance calculation and api server at 8080

--- src/main/java/com/validty/myproject/HomeController.java
+++ src/main/java/com/validty/myproject/HomeController.java
@@ -3,24 +3,29 @@ package com.validty.myproject;
 
 
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.FileNotFoundException;
+
 
 @Controller
 public class HomeController {
 
+    DistanceCalc distanceCalc;
 
-    HomeController(){
-
+    @Autowired
+    HomeController(DistanceCalc distanceCalc){
+        this.distanceCalc =distanceCalc;
     }
 
     @RequestMapping(value = "/customer", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public JSONObject homeHandler() {
-        JSONObject result = null;
+    public JSONObject homeHandler() throws FileNotFoundException {
+        JSONObject result = distanceCalc.distanceCalculation();
         return result;
     }
 }
